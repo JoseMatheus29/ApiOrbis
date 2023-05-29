b7683b660590ac71361e48952d31b1fd5f737a27
JoseMatheus29 <mateus.alvino.101@gmail.com>
2023-05-29 01:10:21 -0300

    Adicionando Crud Stage

---
+++ app/Http/Controllers/Api/StageController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Stage;
+
+class StageController extends Controller
+{
+    public function add(Request $request){
+        try{
+            $stage = new Stage();
+
+
+            $stage->id = $request->nome;
+            $stage->name_pt = $request->name_pt;
+            $stage->name_en = $request->name_en;
+            $stage->description = $request->description;
+
+            $stage->save();
+
+            return ['status' => 'ok'];
+
+        }catch(\Exception $erro){
+            
+            return ['status' => 'erro', 'details' => $erro];
+
+        }
+
+    }
+    public function listAll(){
+        try{
+
+            $stages = Stage::all();
+            return $stages;
+
+        }catch(\Exception $erro){
+            
+            return ['status' => 'erro', 'details' => $erro];
+
+        }
+    }
+    public function listOne($id){
+        try{
+
+            $stages = Stage::find($id);
+            return $stages;
+
+        }catch(\Exception $erro){
+            
+            return ['status' => 'erro', 'details' => $erro];
+
+        }
+    }
+    public function update(Request $request, $id){
+        try{
+
+            $stage = Stage::find($id);
+
+            $stage->id = $request->nome;
+            $stage->name_pt = $request->name_pt;
+            $stage->name_en = $request->name_en;
+            $stage->description = $request->description;
+
+            $stage -> save();
+
+            return ['retorno' => 'ok', 'data' => $request->all()];
+
+        }catch(\Exception $erro){
+            
+            return ['status' => 'erro', 'details' => $erro];
+
+        }
+    }
+    public function delete($id){
+        try{
+
+            $stage = Stage::find($id);
+            $stage->delete();
+
+            return ['status' => 'ok'];
+
+        }catch(\Exception $erro){
+            
+            return ['status' => 'erro', 'details' => $erro];
+
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
-$namespace = 'App\Http\Controllers';
+$namespace = 'App\Http\Controllers\Api';
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\StageController;
+use \App\Http\Controllers\Api\StageController;
 
-Route::get('/readstage',[StageController::class, 'insertStage']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::namespace('Api') -> group(function(){
+
+    Route::post('/addStage',[StageController::class, 'add']);
+    Route::get('/list', [StageController::class, 'listAll']);
+    Route::get('/list/{id}', [StageController::class, 'listOne']);
+    Route::put('/update/{id}', [StageController::class, 'update']);
+    Route::delete('/delete/{id}', [StageController::class, 'delete']);
 });
