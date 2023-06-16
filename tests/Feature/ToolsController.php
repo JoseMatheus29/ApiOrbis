e791d94d40f220c5b96cce875b550e4112f7486f
JoseMatheus29 <mateus.alvino.101@gmail.com>
2023-06-16 12:56:33 -0300

    Começando testes

---
+++ tests/Feature/ToolsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ToolsController extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->postJson('/api/Tools/add', ['name' => 'Sally']);
+ 
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);
+    }
+}
